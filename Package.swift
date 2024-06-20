48d72ea4acb5e0c100d34a971c9b2ceb62648419
Joel-David <git@joeldavid.dev>
2024-06-20 20:50:06 +0800

    bump minimum ios version
    bump minimum ios version

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.7
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -9,7 +9,7 @@ let package = Package(
     // platforms is set so you can include this package in projects that target iOS 13/macOS 10.15/tvOS 13 without
     // getting errors, but the code in it is marked avaliable only for macOS 11 and iOS 14.
     // It compiles and the tests pass on tvOS 14, but I haven't used it in a tvOS app.
-    platforms: [.macOS(.v10_15), .iOS(.v13), .watchOS(.v7), .tvOS(.v13)],
+    platforms: [.macOS(.v12), .iOS(.v15), .watchOS(.v8), .tvOS(.v15), .macCatalyst(.v14)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
--- Tests/CloudKitSyncMonitorTests/SyncMonitorTests.swift
+++ Tests/CloudKitSyncMonitorTests/SyncMonitorTests.swift
@@ -11,7 +11,7 @@ import XCTest
 import CoreData
 @testable import CloudKitSyncMonitor
 
-@available(iOS 14.0, macCatalyst 14.0, OSX 11, tvOS 14.0, *)
+@available(iOS 15.0, macCatalyst 14.0, OSX 11, tvOS 15.0, *)
 final class SyncMonitorTests: XCTestCase {
     func testCanDetectImportError() {
         // Given an active network connection
